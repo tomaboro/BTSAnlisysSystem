7522dccaaf6cd5192700ddb37b10bc9daf0a6b65
tomaboro <motek@student.agh.edu.pl>
2018-12-10 00:28:39 +0100

    Linux Kafka startup scritp fixed

--- Messaging/start_kafka.sh
+++ Messaging/start_kafka.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-./ZooKeeper/bin/zkServer.sh
+sudo ./ZooKeeper/bin/zkServer.sh start
 sleep 10
-./Kafka/bin/kafka-server-start.sh ./Kafka/config/server.properties
+sudo ./Kafka/bin/kafka-server-start.sh ./Kafka/config/server.properties
 sleep 10
-./Kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic event-topic
+sudo ./Kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic event-topic
