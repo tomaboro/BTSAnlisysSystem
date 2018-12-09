3433a679338420dd470bdda263710972349f2d24
Krzysiek <vzxc@student.agh.edu.pl>
2018-12-09 22:33:32 +0100

    Added some places.

--- BTSEvents/src/main/java/utils/Places.java
+++ BTSEvents/src/main/java/utils/Places.java
@@ -13,10 +13,22 @@ public abstract class Places {
 
     static {
         hotels = new HashMap<>();
+        SimpleLocation hotelLocation = new SimpleLocation(0,0);
+        hotels.put(hotelLocation,new Place(hotelLocation, Place.LocationType.hotel,"Hotel Krakowiak"));
         hostels = new HashMap<>();
+        SimpleLocation hostelLocation = new SimpleLocation(0,1);
+        hotels.put(hostelLocation,new Place(hostelLocation, Place.LocationType.hotel,"Bison Hostel"));
         trainStations = new HashMap<>();
+        SimpleLocation trainStationLocation = new SimpleLocation(0,2);
+        hotels.put(trainStationLocation,new Place(trainStationLocation, Place.LocationType.hotel,"Dworzec Kolejowy Kraków"));
         busStations = new HashMap<>();
+        SimpleLocation busStationLocation = new SimpleLocation(0,3);
+        hotels.put(busStationLocation,new Place(busStationLocation, Place.LocationType.hotel,"Dworzec Autobusowy Kraków"));
         airports = new HashMap<>();
+        SimpleLocation airportLocation = new SimpleLocation(0,4);
+        hotels.put(airportLocation,new Place(airportLocation, Place.LocationType.hotel,"Lotnisko Balice"));
         monuments = new HashMap<>();
+        SimpleLocation monumentLocation = new SimpleLocation(0,5);
+        hotels.put(monumentLocation,new Place(monumentLocation, Place.LocationType.hotel,"Wawel"));
     }
 }
