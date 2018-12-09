8bc9cae19795446f473a20bd53b007962c4fb483
Krzysiek <vzxc@student.agh.edu.pl>
2018-12-09 22:11:17 +0100

    Added localDateTime to BTSEvents.

--- BTSEvents/src/main/java/BTSEvents/BTSEvent.java
+++ BTSEvents/src/main/java/BTSEvents/BTSEvent.java
@@ -3,16 +3,19 @@ package BTSEvents;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Getter
 public abstract class BTSEvent implements Serializable {
     private final String id;
     private final double longitude;
     private final double latitude;
+    private final LocalDateTime localDateTime;
 
-    public BTSEvent(String id, double longitude, double latitude) {
+    public BTSEvent(String id, double longitude, double latitude, LocalDateTime localDateTime) {
         this.id = id;
         this.longitude = longitude;
         this.latitude = latitude;
+        this.localDateTime = localDateTime;
     }
 }
